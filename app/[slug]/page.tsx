cab845fa8c9ebe3eaf0e0e32b141ef2ca1be2fab
Phoenixhary <borokinniburhan@gmail.com>
2024-09-16 09:21:08 +0100

    fetched data from wix for the slug single page

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,62 +1,64 @@
 import Add from '@/components/Add';
 import CustomizeProducts from '@/components/CustomizeProducts';
 import ProductImages from '@/components/ProductImages';
+import { wixClientServer } from '@/lib/wixClientServer';
+// import { products } from '@wix/stores';
+import { notFound } from 'next/navigation';
 import React from 'react'
 
-const SinglePage = () => {
+const SinglePage = async ({ params }: {params: { slug: string } }) => {
+ 
+    // console.log(params.slug)
+  const wixClient = await wixClientServer()
+
+  const products = await wixClient.products
+  .queryProducts()
+  .eq("slug", params.slug)
+  .find();
+
+  if (!products.items[0]) {
+    return notFound();
+
+  
+  };
+  const product = products.items[0]; 
+
   return (
     <div className='px-4 md:px-8 lg:px-16 xl:px-32 relative flex flex-col lg:flex-row gap-16'>
               {/* IMG */}
         <div className='w-full lg:w-1/2 lg:sticky top-1 h-max p-4'>
-            <ProductImages />
+            <ProductImages items={product.media?.items} />
         </div>
               {/*TEXTS */}
         <div className='flex flex-col gap-6 w-full lg:w-1/2'> 
-            <div className='text-4xl font-medium'> Product Name </div>
+            <div className='text-4xl font-medium'>  {product.name} </div>
             <p className='text-gray-500'>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-               Accusantium consectetur rem modi suscipit vero, quia debitis.
-                Assumenda neque aliquam beatae.
+              {product.description}
             </p>
             <div className='h-[2px] bg-gray-100' />
-              <div className='flex items-center gap-4'>
-               <h1 className='text-xl text-gray-500 line-through'> $59 </h1>
-               <h1 className='font-medium text-2xl'> $46 </h1>
-            </div>
+                {product.price?.price === product.price?.discountedPrice ?
+                 ( <h1 className='font-medium text-2xl'> ${product.price?.price} </h1>) 
+                 : ( 
+                  <div className='flex items-center gap-4'>
+                  <h1 className='text-xl text-gray-500 line-through'> ${product.price?.price} </h1>
+                  <h1 className='font-medium text-2xl'> ${product.price?.discountedPrice} </h1>
+                  </div>
+                )};
             <div className='h-[2px] bg-gray-100' />
               <CustomizeProducts />
               <Add /> 
             <div className='h-[2px] bg-gray-100' />
             
-              <div className='text-sm'>
-                <h2 className='font-medium mb-4'>Title</h2>
-                   <p>
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                   Laboriosam est temporibus unde fugiat iste laborum ipsam nam quibusdam ratione, 
-                   aliquam sed ipsum nesciunt atque harum optio consectetur itaque officia velit!
-                    Aspernatur perferendis omnis obcaecati iure sequi tenetur! Iusto, nesciunt voluptatibus.
-                   </p>
-              </div>
-
-              <div className='text-sm'>
-                <h2 className='font-medium mb-4'>Title</h2>
-                   <p>
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                   Laboriosam est temporibus unde fugiat iste laborum ipsam nam quibusdam ratione, 
-                   aliquam sed ipsum nesciunt atque harum optio consectetur itaque officia velit!
-                    Aspernatur perferendis omnis obcaecati iure sequi tenetur! Iusto, nesciunt voluptatibus.
-                   </p>
-              </div>
-
-              <div className='text-sm'>
-                <h2 className='font-medium mb-4'>Title</h2>
-                   <p>
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                   Laboriosam est temporibus unde fugiat iste laborum ipsam nam quibusdam ratione, 
-                   aliquam sed ipsum nesciunt atque harum optio consectetur itaque officia velit!
-                    Aspernatur perferendis omnis obcaecati iure sequi tenetur! Iusto, nesciunt voluptatibus.
-                   </p>
-              </div>
+             {product.additionalInfoSections?.map( (section:any) =>(
+               
+               <div className='text-sm' key={section.title}>
+               <h2 className='font-medium mb-4'>{section.title}</h2>
+               <p>
+                {section.description}
+               </p>
+               </div>
+              ))
+              };
 
          </div>
   </div>
--- components/ProductImages.tsx
+++ components/ProductImages.tsx
@@ -3,39 +3,39 @@
 import Image from 'next/image';
 import React, { useState } from 'react'
 
-    const images = [
-      {
-        id: 1,
-        url: "https://images.pexels.com/photos/13257111/pexels-photo-13257111.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      },
-      {
-        id: 2,
-        url: "https://images.pexels.com/photos/13257109/pexels-photo-13257109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      },
-      {
-        id: 3,
-        url: "https://images.pexels.com/photos/4226877/pexels-photo-4226877.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      },
-      {
-        id: 4,
-        url: "https://images.pexels.com/photos/4098340/pexels-photo-4098340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      },
-    ];
-const ProductImages = () => {
+    // const images = [
+    //   {
+    //     id: 1,
+    //     url: "https://images.pexels.com/photos/13257111/pexels-photo-13257111.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    //   },
+    //   {
+    //     id: 2,
+    //     url: "https://images.pexels.com/photos/13257109/pexels-photo-13257109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    //   },
+    //   {
+    //     id: 3,
+    //     url: "https://images.pexels.com/photos/4226877/pexels-photo-4226877.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    //   },
+    //   {
+    //     id: 4,
+    //     url: "https://images.pexels.com/photos/4098340/pexels-photo-4098340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    //   },
+    // ];
+const ProductImages = ({ items }: { items: any }) => {
 
         const [index, setIndex] = useState(0)
   return (
     <div className=''>
         <div className='relative h-[500px]'>
-            <Image src={images[index].url}
+            <Image src={items[index].image?.url}
             alt='' fill sizes='50vw' className='object-cover rounded-md'
              />
         </div>
 
         <div className='flex justify-between gap-4 '> 
-        {images.map((img, i) => (
-                 <div className="relative w-1/4 h-32 gap-4 mt-8 cursor-pointer" key={img.id} onClick={() => setIndex(i)}> 
-                 <Image src={img.url}
+        {items.map((item:any, i:number) => (
+                 <div className="relative w-1/4 h-32 gap-4 mt-8 cursor-pointer" key={item.id} onClick={() => setIndex(i)}> 
+                 <Image src={item.image?.url}
                  alt="" fill sizes="30vw" className="object-cover rounded-md"
                   /> 
                   </div>
